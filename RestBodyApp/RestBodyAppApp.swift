a4a7d9d86e746e01b884185cb6c5b197cb88ca78
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-01-15 14:36:49 +0700

    Initial Commit

---
+++ RestBodyApp/RestBodyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestBodyAppApp.swift
+//  RestBodyApp
+//
+//  Created by Putut Yusri Bahtiar on 15/01/23.
+//
+
+import SwiftUI
+
+@main
+struct RestBodyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
